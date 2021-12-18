80a9adfd5bcdefe80caed5e78d1d0d0fa3a56529
jihnkim <sysanal1697@gmail.com>
2021-12-18 18:26:57 +0900

    without else

--- with_python/baekjoon/practice/if2.py
+++ with_python/baekjoon/practice/if2.py
@@ -6,12 +6,10 @@ def solution(x, y):
     if x > 0:
         if y > 0:
             return 1
-        else:
-            return 4
+        return 4
     else:
         if y > 0:
             return 2
-        else:
-            return 3
+        return 3
 
 print(solution(int(x), int(y)))
