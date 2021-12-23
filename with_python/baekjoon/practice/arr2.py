7f0f6ccc225c0a9dfdc07d0deec6ee679a94dacd
jihnkim <sysanal1697@gmail.com>
2021-12-23 17:38:03 +0900

    2562

---
+++ with_python/baekjoon/practice/arr2.py
@@ -0,0 +1,14 @@
+"""
+2562
+"""
+
+arr = []
+for loc in range(1, 10):
+    arr.append([int(input()), loc])
+
+def solution(arr):
+    arr.sort()
+    print(arr[-1][0])
+    print(arr[-1][1])
+
+solution(arr)
