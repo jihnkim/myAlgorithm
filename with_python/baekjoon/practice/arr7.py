04bbcbb6ea675e770c614835751f2e0c3b70118d
jihnkim <sysanal1697@gmail.com>
2021-12-28 14:34:33 +0900

    calc num when over avg

--- with_python/baekjoon/practice/arr7.py
+++ with_python/baekjoon/practice/arr7.py
@@ -1,5 +1,18 @@
 """
-commit check
+4344
+문자열 포매팅 시 자릿수 제한법
+> f'{VALUE:.2f}' 중괄호안의 값을 두자리까지만 출력
 """
 
-tmp = "commit"
+N = int(input())
+
+for _ in range(N):
+    score_lst = list(map(int, input().split()))[1:]
+    n = len(score_lst)
+    avg = sum(score_lst) / n
+
+    cnt = 0
+    for score in score_lst:
+        if score - avg > 0: cnt += 1
+    
+    print(f'{round((cnt / n) * 100, 3):.3f}%')
