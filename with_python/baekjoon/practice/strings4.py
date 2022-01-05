f91bbae06d2417c4e5a4ec0fc1ebcdee2c1df957
jihnkim <sysanal1697@gmail.com>
2022-01-05 10:33:08 +0900

    string iterationpython c:/Users/jih11/Desktop/myprojects/cote/with_python/baekjoon/practice/strings4.py

---
+++ with_python/baekjoon/practice/strings4.py
@@ -0,0 +1,14 @@
+"""
+2675
+문자열 반복
+줄 바꿈시 그냥 상위 반복문에 print() 넣는 방법도 있다
+"""
+
+T = int(input())
+
+for _ in range(T):
+    s = input()
+    i, S = int(s[:1]), s[2:]
+    for spell in S:
+        print(spell * i, end='')
+    print()
